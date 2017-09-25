e07a7fa2fa36e2656b8447120d83649a1ed670f8
eric-elem <eric.elem@andela.com>
2017-09-25 07:11:56 +0300

    ft(items): Add component to display all items

---
+++ src/components/items/items.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ItemModal from './item-modal';
+import Item from './item';
+
+export default function Items(props) {
+  return (
+    <div>
+      <div className="container">
+        <div className="card mt-3 mb-3">
+          <div className="card-block">
+            <div className="d-flex w-100 justify-content-between mb-2">
+              <button onClick={props.viewBucketlists}>{props.selectedBucketlist.title}</button>
+              <button
+                className="btn btn-sm btn-primary col-xs-12"
+                data-toggle="modal"
+                data-target="#addItemModal"
+              ><span className="fa fa-plus pull-left" /> Add Item
+              </button>
+            </div>
+            {
+              props.items && props.items.length !== 0 &&
+                <div>
+                  <h6 className="card-text ml-4 pb-2">
+                    Items <small className="text-muted">select to view details</small>
+                  </h6>
+                  <ul className="list-group list-group-flush">
+                    {
+                      props.items.map(item =>
+                        (<Item
+                          key={item.id}
+                          item={item}
+                          request={props.request}
+                          selectedBucketlist={props.selectedBucketlist}
+                        />
+                        ),
+                      )
+                    }
+                  </ul>
+                </div>
+            }
+          </div>
+        </div>
+      </div>
+      <ItemModal
+        title="Create an item"
+        action="Submit"
+        theId="addItemModal"
+        request={props.request}
+        selectedBucketlist={props.selectedBucketlist}
+      />
+    </div>
+  );
+}
