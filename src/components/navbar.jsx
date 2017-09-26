91ebb8d1d6f191b5f5d83bcc78399d77bb514100
eric-elem <eric.elem@andela.com>
2017-09-26 20:52:40 +0300

    ft(navbar): Add logout dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,14 +15,14 @@ export default function NavBar(props) {
     }
   }
   return (
-    <div className="container">
+    <div className="">
       <div className="row">
         <div className="col-md-4">
           <a href="" onClick={viewBucketlists} className="navbar-brand logo text-white">
-            <h3><span className="fa fa-lg fa-bitbucket mr-3" />| &nbsp; BUCKETLISTS</h3>
+            <h3><span className="fa fa-lg fa-bitbucket mr-3 ml-3" />| &nbsp; Bucketlists</h3>
           </a>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 text-center">
           <input
             className="form-control mr-sm-2 search-box"
             type="text"
@@ -31,13 +31,14 @@ export default function NavBar(props) {
           />
         </div>
         <div className="col-md-4 text-right">
-          <a
-            className="nav-link logo text-white"
-            href=""
-            onClick={logoutUser}
-          >
-            { localStorage.getItem('username') } &nbsp;<span className="fa fa-sign-out" />
-          </a>
+          <div className="dropdown logout-dropdown">
+            <div className="fa fa-user-circle fa-lg text-white dropdown-toggle" id="dropdownMenuLink" data-toggle="dropdown">
+              &nbsp;<span className="logo"><b> { localStorage.getItem('username') } </b></span>
+            </div>
+            <div className="dropdown-menu dropdown-cust" aria-labelledby="dropdownMenuLink">
+              <a className="dropdown-item" href="" onClick={logoutUser}>Logout</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
